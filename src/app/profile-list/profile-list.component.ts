419dd2b223452b1d29cac9e04c21ad935d499faf
sheiniwash <shriniwashsharma98413@gmail.com>
2021-02-10 12:28:55 +0545

    profile

--- src/app/profile-list/profile-list.component.ts
+++ src/app/profile-list/profile-list.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProfileListService } from "./profile-list.service"
 @Component({
   selector: 'app-profile-list',
   templateUrl: './profile-list.component.html',
   styleUrls: ['./profile-list.component.css']
 })
 export class ProfileListComponent implements OnInit {
+  userList:any[];
+  constructor(private profileService:ProfileListService) {
+    
+    this.userList  = [];
 
-  constructor() { }
+    this.profileService.getUserList().subscribe(user => {
+      this.userList = user.data;
+    })
+
+   }
 
   ngOnInit(): void {
   }
--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileListService } from "../profile-list/profile-list.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-profile-view',
   templateUrl: './profile-view.component.html',
   styleUrls: ['./profile-view.component.css']
 })
-export class ProfileViewComponent implements OnInit {
 
-  constructor() { }
+
+export class ProfileViewComponent implements OnInit {
+  profile:any;
+  constructor(
+    private profileService:ProfileListService, 
+    private route:ActivatedRoute) {
+
+      this.profile = {
+        avatar: "",
+        email: "",
+        first_name: "",
+        id: null,
+        last_name: ""
+      }
+
+    this.route.params.subscribe(params => {
+      console.log(params);
+      this.profileService.getSingleUser(params.id).subscribe(user => {
+        console.log(user);
+        this.profile =  user.data;
+      })
+    })
+
+   }
 
   ngOnInit(): void {
   }
